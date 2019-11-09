4f9f61b9b61be64d70f7c8a3a5bb39c925aa438d
NyuB <decaeste@etud.insa-toulouse.fr>
2019-11-09 11:03:11 +0100

    Corrected oerder of settings to activate clock before setting timer register (previous order would work in simulation but not on real device)

--- ProjKEIL_Chrono_TIM_IT_LL/Components/Ecoute.c
+++ ProjKEIL_Chrono_TIM_IT_LL/Components/Ecoute.c
@@ -1,19 +1,18 @@
 #include "Ecoute.h"
-
 //Constantes à vérifier
 const double ANGLE_MAX = 180;
 const double ANGLE_MIN = 0;
 const double TH_MAX = 0.002;
 const double TH_MIN = 0.001;
-
 PWM_TypeDef pwm_ecoute;
-void conf_pwm_ecoute(){
+void conf_pwm_ecoute(){//Timer 1;Port A8;Channel 1;50Hz
 	RCC->APB2ENR |= RCC_APB2ENR_IOPAEN;
 	LL_GPIO_SetPinMode(GPIOA,LL_GPIO_PIN_8,LL_GPIO_MODE_ALTERNATE);
 	LL_GPIO_SetPinOutputType(GPIOA,LL_GPIO_PIN_8,LL_GPIO_OUTPUT_PUSHPULL); // ATTENTION
+	pwm_ecoute=init_PWM(TIM1,50,1);
 	TIM1->CCER |= TIM_CCER_CC1E;
 	TIM1->BDTR |= TIM_BDTR_MOE;
-	pwm_ecoute=init_PWM(TIM1,50,1);
+	
 }
 void set_angle_ecoute(double theta){
 	double period = TH_MIN + (theta/(ANGLE_MAX-ANGLE_MIN))*(TH_MAX-TH_MIN); //On ramène l'intervalle angulaire sur l'intervalle temporel.
--- ProjKEIL_Chrono_TIM_IT_LL/Components/Ecoute.h
+++ ProjKEIL_Chrono_TIM_IT_LL/Components/Ecoute.h
@@ -1,8 +1,9 @@
+
+#ifndef ECOUTE_H
+#define ECOUTE_H
 #include "PWM_Driver.h"
 #include "stm32f1xx_ll_tim.h" 
 #include "stm32f1xx_ll_gpio.h"
-#ifndef ECOUTE_H
-#define ECOUTE_H
 void conf_pwm_ecoute(void);
 void set_angle_ecoute(double theta);
 #endif

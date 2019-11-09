385dfd526ac89622555072f8e6132f3a77b5f2ea
NyuB <decaeste@etud.insa-toulouse.fr>
2019-11-09 11:04:30 +0100

    Implemented PWM configuration and power control function for the Plate servomotor

--- ProjKEIL_Chrono_TIM_IT_LL/Components/Plate.h
+++ ProjKEIL_Chrono_TIM_IT_LL/Components/Plate.h
@@ -1,6 +1,8 @@
 #ifndef PLATE_H
 #define PLATE_H
-
-
-
+#include "PWM_Driver.h"
+#include "stm32f1xx_ll_tim.h" 
+#include "stm32f1xx_ll_gpio.h"
+void conf_pwm_plate(void);//Timer 2;Port A1;Channel 2;50Hz
+void set_servo_mean_voltage(double mean_voltage);//Set the PWM high period to generate a mean signal of [mean_voltage]V
 #endif

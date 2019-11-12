3972cefc3f4b910d4ab5ef22e5231dd957e520c5
Brice Decaestecker <decaeste@insat.insa-toulouse.fr>
2019-11-12 18:11:30 +0100

    Prototype of RX_Rcv component

---
+++ ProjKEIL_Chrono_TIM_IT_LL/Components/RX_Rcv.c
@@ -0,0 +1,11 @@
+#include "RX_Rcv.h"
+
+double get_pw_in_ratio(void){
+	return 0.0; //TODO
+};
+char get_orientation(void){
+	return 0; //TODO
+}
+void conf_pwm_in(void){
+	return; //TODO
+}
--- ProjKEIL_Chrono_TIM_IT_LL/Components/RX_Rcv.h
+++ ProjKEIL_Chrono_TIM_IT_LL/Components/RX_Rcv.h
@@ -1,6 +1,8 @@
 #ifndef RX_RCV_H
 #define RX_RCV_H
 
-
+double get_pw_in_ratio(void);
+char get_orientation(void);
+void conf_pwm_in(void);
 
 #endif
